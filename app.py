cc4ae263403ef6b5121a319daefe905e1a0ba03c
David Weiss <dweiss2@gmail.com>
2022-02-04 10:37:17 -0600

    Fixed more typos

--- app.py
+++ app.py
@@ -4,28 +4,20 @@ import re
 from pathlib import Path
 from typing import List, Set, Tuple
 
+import colorama
+
 import word
 
-try:
-    import colorama
-    colorama.init()
-    COLOR_MAP = {
-        "1": (word.Color.BLACK, colorama.Fore.WHITE),
-        "b": (word.Color.BLACK, colorama.Fore.WHITE),
-        "2": (word.Color.YELLOW, colorama.Fore.YELLOW),
-        "y": (word.Color.YELLOW, colorama.Fore.YELLOW),
-        "3": (word.Color.GREEN, colorama.Fore.GREEN),
-        "g": (word.Color.GREEN, colorama.Fore.GREEN),
-    }
-except ImportError:
-    COLOR_MAP = {
-        "1": (word.Color.BLACK, ""),
-        "b": (word.Color.BLACK, ""),
-        "2": (word.Color.YELLOW, ""),
-        "y": (word.Color.YELLOW, ""),
-        "3": (word.Color.GREEN, ""),
-        "g": (word.Color.GREEN, ""),
-    }
+colorama.init()
+
+COLOR_MAP = {
+    "1": (word.Color.BLACK, colorama.Fore.WHITE),
+    "b": (word.Color.BLACK, colorama.Fore.WHITE),
+    "2": (word.Color.YELLOW, colorama.Fore.YELLOW),
+    "y": (word.Color.YELLOW, colorama.Fore.YELLOW),
+    "3": (word.Color.GREEN, colorama.Fore.GREEN),
+    "g": (word.Color.GREEN, colorama.Fore.GREEN),
+}
 
 MAX_GUESSES = 6
 WORD_LENGTH = 5
